d93d5d84efacc840fac4d1d602e3fc2c0485eca8
Kent Åsvang <kentaasvang@gmail.com>
2021-02-19 11:26:28 +0100

    init

--- BedriftsrådetBodø/Pages/Index.cshtml.cs
+++ BedriftsrådetBodø/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace BedriftsrådetBodø.Pages
+namespace LynxPublishing.Pages
 {
     public class IndexModel : PageModel
     {
@@ -19,7 +19,6 @@ namespace BedriftsrådetBodø.Pages
 
         public void OnGet()
         {
-
         }
     }
 }
